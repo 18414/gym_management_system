34ab96896a38ceecaf3ba72a7765519a40826723
Bhushan Mahajan <bmahajan0@gmail.com>
2019-04-07 03:16:33 +0000

    2019-04-07

--- ec2_gym_system_infra.sh
+++ ec2_gym_system_infra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Version: 1.0
 
-#Deleting previous ip addresses of instances from ANSIBLE inventory
+#Deleting previous ip addresses of instances from ANSIBLE inventory to avoid inconvenience
 sed -i '/^[0-9]/d' hosts
 sleep 1
 
@@ -15,6 +16,14 @@ grep [0-9] hosts > ip_list
 
 sleep 1
 
+
+
+#inserting tables
+#sudo mysql -h gymsystemdb.czrdtrac0wnc.us-east-2.rds.amazonaws.com -u bhushan -p ganesha123 < gym_management_system.sql
+
 #Launching containers 
-ansible-playbook docker_launch_xampp_v2_new.yml --extra-vars "instance1=`cat ip_list| head -1` instance2=`cat ip_list| tail -1` paswd=ganesha@123"
+ansible-playbook docker_launch_xampp_v3_new.yml --extra-vars "instance1=`cat ip_list| head -1` instance2=`cat ip_list| tail -1` paswd=ganesha@123 db_host=$db_hostname"
+
+
+
 
